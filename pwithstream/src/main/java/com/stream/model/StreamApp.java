864787b75ff5d8c5b60de5fbfe7be922e111c78d
CHIHAOUIKAIS <kais.chihaoui@esprit.tn>
2020-05-10 12:14:17 +0200

    pwj

---
+++ pwithstream/src/main/java/com/stream/model/StreamApp.java
@@ -0,0 +1,35 @@
+package com.stream.model;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamApp {
+
+	
+	public List<Trainer> sortedTrainerByAge(Trainer...object){
+		List<Trainer> trainers = Arrays.asList(object);
+		
+		List<Trainer> trainersSorted = trainers.stream()
+				.sorted(Comparator.comparingInt(Trainer::getAge))
+				.collect(Collectors.toList());
+		
+		return trainersSorted;
+	}
+	
+	
+	public List<String> sortedListFunc(String... a) {
+		List<String> list = Arrays.asList(a);
+		List<String> sortedList = list.stream().sorted().collect(Collectors.toList());
+		return sortedList;
+	}
+
+	public List<String> reverseOrderFunc(String... b) {
+
+		List<String> list = Arrays.asList(b);
+		List<String> reverseList = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+		return reverseList;
+	}
+
+}
---
+++ pwithstream/src/main/java/com/stream/model/StreamAppMain.java
@@ -0,0 +1,22 @@
+package com.stream.model;
+
+public class StreamAppMain {
+
+	public static void main(String[] args) {
+
+		StreamApp sapp = new StreamApp();
+
+		System.out.println(":: JAVA_STATIC_MAIN_TEST ::");
+
+		sapp.sortedListFunc("A", "1", "E", "C", "3", "2", "B", "F").forEach((System.out::println));
+		System.out.println("-> :: result :: JAVA 8_11_14");
+
+		sapp.reverseOrderFunc("A", "1", "E", "C", "3", "2", "B", "F").forEach(System.out::println);
+		System.out.println("-> :: result :: JAVA 8_11_14");
+
+		sapp.sortedTrainerByAge(new Trainer("kais", 30), new Trainer("Jessy", 22), new Trainer("Ahmed", 25),
+				new Trainer("kONE", 29)).forEach(System.out::println);
+
+	}
+
+}
